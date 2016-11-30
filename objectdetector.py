c076f7d6c8daeb0e0222dd7b9d939386af145013
asanthamax <asanthathilina@gmail.com>
2016-11-29 20:44:25 -0800

    yolo implementation added

--- objectdetector.py
+++ objectdetector.py
@@ -1,140 +1,154 @@
+import keras
 import numpy
 import cv2
 import PIL
 import matplotlib.pyplot as plt
-from PIL import Image
-from keras.applications import VGG16
-from keras.callbacks import EarlyStopping
-from keras.constraints import maxnorm
+from PIL import Image, ImageDraw, ImageFont
 from keras.engine import Layer, merge, Merge, Input, Model
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from matplotlib import pyplot
-from pip.req.req_file import process_line
-from scipy.misc import toimage
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, MaxPooling2D, Reshape, Permute, Activation, Dropout
-from keras.optimizers import RMSprop, SGD
+from keras.layers import Dense, Flatten, MaxPooling2D, Reshape, Permute, Activation, Dropout, LeakyReLU
+from keras.optimizers import RMSprop, SGD, Adam
 from keras.layers.convolutional import Convolution2D, ZeroPadding2D, UpSampling2D, Deconvolution2D, Cropping2D
-from keras.utils import np_utils
 from keras import backend as k
+
+from objectpreprocessor import custom_loss, LossHistory, TestAcc, generate_batch_data
 
 k.set_image_dim_ordering('th')
 
 seed = 7
 numpy.random.seed(seed)
-train_directory = 'data/train'
-test_directory = 'data/test'
+train_directory = 'data/train/train'
+test_directory = 'data/test/test'
 filepath = "data/models"
-train_datagen = ImageDataGenerator(
 
-            rescale=1./255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True
-        )
-train_data_generator = train_datagen.flow_from_directory(
-
-            train_directory,
-            target_size=(150, 150),
-            batch_size=96,
-            class_mode=None
-      )
-
-
-def train_generator():
-    for a in train_data_generator:
-        yield a, a
-
-
-test_datagen = ImageDataGenerator(
-
-            rescale=1./255
-        )
-
-test_data_generator = test_datagen.flow_from_directory(
-
-    test_directory,
-    target_size=(150, 150),
-    batch_size=96,
-    class_mode=None
-)
-
-
-def test_generator():
-    for b in test_data_generator:
-        yield b, b
 
 input_img = Input(shape=(3, 150, 150))
-cnn1 = Convolution2D(96, 3, 3, activation='relu', border_mode='same', name='input')(input_img)
-cnn2 = Convolution2D(96, 3, 3, activation='relu', border_mode='same', name="conv_net1")(cnn1)
-pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(cnn2)
+cnn = Convolution2D(16, 2, 2, border_mode='same')(input_img)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-cnn3 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='conv_net2_1')(pool1)
-cnn4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='conv_net2_2')(cnn3)
-pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2')(cnn4)
+cnn = Convolution2D(32, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-cnn5 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net3_1')(pool2)
-cnn6 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net3_2')(cnn5)
-cnn7 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net3_3')(cnn6)
-pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3')(cnn7)
+cnn = Convolution2D(64, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-cnn8 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net4_1')(pool3)
-cnn9 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net4_2')(cnn8)
-cnn10 = Convolution2D(384, 3, 3, activation='relu', border_mode='same', name='conv_net4_3')(cnn9)
-pool4 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool4')(cnn10)
+cnn = Convolution2D(128, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-cnn11 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='conv_net5_1')(pool4)
-cnn12 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='conv_net5_2')(cnn11)
-cnn13 = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='conv_net5_3')(cnn12)
-pool5 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool5')(cnn13)
+cnn = Convolution2D(256, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-cnn14 = Convolution2D(4096, 1, 1, activation='relu', border_mode='same', name='conv_net6_1')(pool5)
-dropout1 = Dropout(0.5)(cnn14)
-cnn15 = Convolution2D(4096, 1, 1, activation='relu', border_mode='same', name='conv_net7_2')(dropout1)
-dropout2 = Dropout(0.5)(cnn15)
-cnn16 = Convolution2D(21, 1, 1, activation='relu', border_mode='same', name='conv_net8_3')(dropout2)
+cnn = Convolution2D(512, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(cnn)
 
-decnn1 = Deconvolution2D(21, 1, 1, output_shape=(None, 21, 8, 8), border_mode='valid', subsample=(2, 2))(cnn16)
-crop1 = Cropping2D(cropping=((2, 2), (2, 2)))(decnn1)
+cnn = Convolution2D(1024, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
 
-X = Convolution2D(21, 1, 1, activation='relu', border_mode='same', name='conv_net8_1')(pool5)
-crop2 = Cropping2D(cropping=((2, 2), (2, 2)))(X)
-merging = merge([crop1, X], mode='sum')
 
-decnn2 = Deconvolution2D(21, 16, 16, output_shape=(None, 21, 158, 158), border_mode='valid', subsample=(8, 8))(merging)
-crop3 = Cropping2D(cropping=((4, 4), (4, 4)))(decnn2)
-reshape = Reshape((21, 150*150))(crop3)
-activation = Activation('softmax')(reshape)
-model = Model(input_img, activation)
+cnn = Convolution2D(1024, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
 
+cnn = Convolution2D(1024, 2, 2, border_mode='same')(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+
+cnn = Flatten()(cnn)
+cnn = Dense(4096)(cnn)
+cnn = LeakyReLU(alpha=0.1)(cnn)
+cnn = Dense(7*7*25, activation='sigmoid')(cnn)
+
+model = Model(input_img, cnn)
 epochs = 100
 learning_rate = 0.001
 decay = learning_rate / epochs
 sgd = SGD(lr=learning_rate, decay=decay, momentum=0.9, nesterov=True)
 rms = RMSprop(lr=learning_rate, decay=decay)
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(loss=custom_loss, optimizer=sgd, metrics=['accuracy'])
 print(model.summary())
-history = model.fit_generator(
-    train_generator(),
-    samples_per_epoch=256,
-    nb_epoch=100,
-    validation_data=test_generator(),
-    nb_val_samples=4
-)
 
-#model.load_weights(filepath=filepath+"/objectdetector.h5")
-file = open(filepath+"/objectdetector.h5", 'a')
-model.save_weights(filepath=filepath+"/objectdetector.h5")
-file.close()
+#history = LossHistory()
+#testAcc = TestAcc()
 
-img = Image.open("data/test/shadows/IMG_2157.jpg")
+#history = model.fit_generator(
+#    generate_batch_data('data/VOC2012', 'data/VOC2012/ImageSets/Segmentation/trainval.txt', 16, sample_number=2913),
+#    samples_per_epoch=5826,
+#    nb_epoch=40,
+#    callbacks=[history, testAcc]
+#)
+
+file = open(filepath+"/recognizer.h5", 'a')
+#model.save_weights(filepath=filepath+"/recognizer.h5")
+model.load_weights(filepath=filepath+"/recognizer.h5")
+img = Image.open("data/VOC2012/JPEGImages/2007_000733.jpg")
 img = img.resize((150, 150), PIL.Image.ANTIALIAS)
-img.save("data/test/shadows/IMG_2157.jpg")
+img.save("data/VOC2012/JPEGImages/2007_000733.jpg")
 # i = cv2.imread("data/train/shadows/test/nonshadows/04822305_257_1025_513_1281.jpg")
 # cv2.putText(i, "Shadow Detected", (70, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 # cv2.imshow("Object", i)
 # cv2.waitKey(100000)
-testImage = load_img("data/test/shadows/IMG_2157.jpg")
+testImage = load_img("data/VOC2012/JPEGImages/2007_000733.jpg")
 x = img_to_array(testImage)
 x = x.reshape((1,)+x.shape)
-model.predict(x)
+labels = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train","tvmonitor"]
+img = Image.open("data/VOC2012/JPEGImages/2007_000733.jpg")
+img = img.resize((150, 150), PIL.Image.ANTIALIAS)
+img.save("data/VOC2012/JPEGImages/2007_000733.jpg")
+drawImage = Image.open("data/VOC2012/JPEGImages/2007_000733.jpg")
+drawer = ImageDraw.Draw(drawImage)
+prediction = model.predict(x)
+prediction = prediction[0]
+final_output = []
+for i in range(49):
+    thresh = 0.3
+    out = prediction[i*25: (i+1)* 25]
+    if(out[24] > thresh):
+
+        row = i/7
+        col = i/7
+        centerx = 64 * col + 64 * prediction[0]
+        centery = 64 * row + 64 * prediction[1]
+        h = prediction[3] * prediction[3]
+        h = h * 150
+        w = prediction[4] * prediction[4]
+        w = w * 150
+        left = centerx - w/2.0
+        right = centerx + w/2.0
+        up = centery - h/2.0
+        down = centery + h/2.0
+
+
+        if(left < 0):left = 0
+        if(right > 150):right = 150
+        if(up < 0):up = 0
+        if(down > 150):down = 150
+
+        drawer.rectangle([left, up, right, down], outline='red')
+        if(left == 0):
+            xcoordinate = 0
+        else:
+            xcoordinate = left - 1
+        if(up == 0):
+            ycoordinate = 0
+        else:
+            ycoordinate = up - 1
+
+        final_output.append(left)
+        final_output.append(right)
+        final_output.append(up)
+        final_output.append(down)
+        final_output.append(numpy.argmax(prediction[2:24]))
+        drawer.text((xcoordinate, ycoordinate), labels[numpy.argmax(prediction[2:24])])
+drawImage.save("data/VOC2012/JPEGImages/prediction1.jpg")
+i = cv2.imread("data/VOC2012/JPEGImages/prediction1.jpg")
+cv2.imshow("Object", i)
+cv2.waitKey(100000)
+print(model.predict(x))
+print(final_output)
+
---
+++ objectpreprocessor.py
@@ -0,0 +1,334 @@
+import keras
+import numpy
+import xml.etree.ElementTree as ET
+import theano.tensor as T
+from PIL import Image
+from scipy import misc
+
+
+def crop_detection(imPath,new_width=150,new_height=150,save=False,test=False):
+    im = Image.open(imPath)
+    im = im.resize((new_width,new_height),Image.ANTIALIAS)
+
+    image_array = numpy.array(im)
+    image_array = numpy.rollaxis(image_array,2,0)
+    image_array = image_array/255.0
+    image_array = image_array * 2.0 - 1.0
+
+    if(test):
+        image_array = (image_array + 1.0) / 2.0 * 225.0
+        image_array = numpy.rollaxis(image_array,2,0)
+        image_array = numpy.rollaxis(image_array,2,0)
+
+        misc.imsave('recovered.jpg', image_array)
+
+    if(save):
+        return image_array,im
+    else:
+        return image_array
+
+
+
+class objInfo():
+    """
+    objInfo saves the information of an object, including its class num, its cords
+    """
+    def __init__(self,x,y,h,w,class_num):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
+        self.class_num = class_num
+
+
+class Cell():
+    """
+    A cell is a grid cell of an image, it has a boolean variable indicating whether there are any objects in this cell,
+    and a list of objInfo objects indicating the information of objects if there are any
+    """
+    def __init__(self):
+        self.has_obj = False
+        self.objs = []
+
+class image():
+    """
+    Args:
+       side: An image is divided into side*side grids
+    Each image class has two variables:
+       imgPath: the path of an image on my computer
+       bboxes: a side*side matrix, each element in the matrix is cell
+    """
+    def __init__(self,side,imgPath):
+        self.imgPath = imgPath
+        self.boxes = []
+        for i in range(side):
+            rows = []
+            for j in range(side):
+                rows.append(Cell())
+            self.boxes.append(rows)
+
+    def parseXML(self,xmlPath,labels,side):
+        """
+        Args:
+          xmlPath: The path of the xml file of this image
+          labels: label names of pascal voc dataset
+          side: an image is divided into side*side grid
+        """
+        tree = ET.parse(xmlPath)
+        root = tree.getroot()
+
+        width = int(root.find('size').find('width').text)
+        height = int(root.find('size').find('height').text)
+
+        for obj in root.iter('object'):
+            class_num = labels.index(obj.find('name').text)
+            bndbox = obj.find('bndbox')
+            xmin = int(bndbox.find('xmin').text)
+            ymin = int(bndbox.find('ymin').text)
+            xmax = int(bndbox.find('xmax').text)
+            ymax = int(bndbox.find('ymax').text)
+            h = ymax-ymin
+            w = xmax-xmin
+            #objif = objInfo(xmin/448.0,ymin/448.0,np.sqrt(ymax-ymin)/448.0,np.sqrt(xmax-xmin)/448.0,class_num)
+
+            #which cell this obj falls into
+            centerx = (xmax+xmin)/2.0
+            centery = (ymax+ymin)/2.0
+            newx = (150.0/width)*centerx
+            newy = (150.0/height)*centery
+
+            h_new = h * (150.0 / height)
+            w_new = w * (150.0 / width)
+
+            cell_size = 150.0/side
+            col = int(newx / cell_size)
+            row = int(newy / cell_size)
+           # print "row,col:",row,col,centerx,centery
+
+            cell_left = col * cell_size
+            cell_top = row * cell_size
+            cord_x = (newx - cell_left) / cell_size
+            cord_y = (newy - cell_top)/ cell_size
+
+            objif = objInfo(cord_x, cord_y, numpy.sqrt(h_new/150.0), numpy.sqrt(w_new/150.0),class_num)
+            self.boxes[row][col].has_obj = True
+            self.boxes[row][col].objs.append(objif)
+
+def prepareBatch(start,end,imageNameFile,vocPath):
+    """
+    Args:
+      start: the number of image to start
+      end: the number of image to end
+      imageNameFile: the path of the file that contains image names
+      vocPath: the path of pascal voc dataset
+    Funs:
+      generate a batch of images from start~end
+    Returns:
+      A list of end-start+1 image objects
+    """
+    imageList = []
+    labels = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train","tvmonitor"]
+    file = open(imageNameFile)
+    imageNames = file.readlines()
+    for i in range(start,end):
+        imgName = imageNames[i].strip('\n')
+        imgPath = vocPath+'/JPEGImages/'+imgName+'.jpg'
+        xmlPath = vocPath+'/Annotations/'+imgName+'.xml'
+        img = image(side=7, imgPath=imgPath)
+        img.parseXML(xmlPath, labels, 7)
+        imageList.append(img)
+
+    return imageList
+
+
+def generate_batch_data(vocPath,imageNameFile,batch_size,sample_number):
+    """
+    Args:
+      vocPath: the path of pascal voc data
+      imageNameFile: the path of the file of image names
+      batchsize: batch size, sample_number should be divided by batchsize
+    Funcs:
+      A data generator generates training batch indefinitely
+    """
+    class_num = 20
+    #Read all the data once and dispatch them out as batches to save time
+    TotalimageList = prepareBatch(0,sample_number,imageNameFile,vocPath)
+
+    while 1:
+        batches = sample_number // batch_size
+        for i in range(batches):
+            images = []
+            boxes = []
+            sample_index = numpy.random.choice(sample_number,batch_size,replace=True)
+            #sample_index = [3]
+            for ind in sample_index:
+                image = TotalimageList[ind]
+                #print image.imgPath
+                image_array = crop_detection(image.imgPath,new_width=150,new_height=150)
+                #image_array = np.expand_dims(image_array,axis=0)
+
+                y = []
+                for i in range(7):
+                    for j in range(7):
+                        box = image.boxes[i][j]
+                        '''
+                        ############################################################
+                        #x,y,h,w,one_hot class label vector[0....0],objectness{0,1}#
+                        ############################################################
+                        '''
+                        if(box.has_obj):
+                            obj = box.objs[0]
+
+                            y.append(obj.x)
+                            y.append(obj.y)
+                            y.append(obj.h)
+                            y.append(obj.w)
+
+                            labels = [0]*20
+                            labels[obj.class_num] = 1
+                            y.extend(labels)
+                            y.append(1) #objectness
+                        else:
+                            y.extend([0]*25)
+                y = numpy.asarray(y)
+                #y = np.reshape(y,[1,y.shape[0]])
+
+                images.append(image_array)
+                boxes.append(y)
+            #return np.asarray(images),np.asarray(boxes)
+            yield numpy.asarray(images), numpy.asarray(boxes)
+
+
+def custom_loss(y_true,y_pred):
+    '''
+    Args:
+      y_true: Ground Truth output
+      y_pred: Predicted output
+      The forms of these two vectors are:
+      ######################################
+      ## x,y,h,w,p1,p2,...,p20,objectness ##
+      ######################################
+    Returns:
+      The loss caused by y_pred
+    '''
+    y1 = y_pred
+    y2 = y_true
+    loss = 0.0
+
+    scale_vector = []
+    scale_vector.extend([2]*4)
+    scale_vector.extend([1]*20)
+    scale_vector = numpy.reshape(numpy.asarray(scale_vector),(1,len(scale_vector)))
+
+    for i in range(49):
+        y1_piece = y1[:,i*25:i*25+24]
+        y2_piece = y2[:,i*25:i*25+24]
+
+        y1_piece = y1_piece * scale_vector
+        y2_piece = y2_piece * scale_vector
+
+        loss_piece = T.sum(T.square(y1_piece - y2_piece),axis=1)
+        loss = loss + loss_piece * y2[:,i*25+24]
+        loss = loss + T.square(y2[:,i*25+24] - y1[:,i*25+24])
+
+    #loss = T.sum(loss)
+    loss = T.sum(loss)
+    return loss
+
+
+class LossHistory(keras.callbacks.Callback):
+    '''
+    Use LossHistory to record loss
+    '''
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
+def Acc(imageList,model,sample_number=5000,thresh=0.3):
+    correct = 0
+    object_num = 0
+
+    count = 0
+    for image in imageList:
+        count += 1
+        #Get prediction from neural network
+        img = crop_detection(image.imgPath,new_width=150,new_height=150)
+        img = numpy.expand_dims(img, axis=0)
+        out = model.predict(img)
+        out = out[0]
+
+        for i in range(49):
+            preds = out[i*25:(i+1)*25]
+            if(preds[24] > thresh):
+                object_num += 1
+                row = int(i/7)
+                col = int(i%7)
+                class_num = numpy.argmax(preds[4:24])
+
+                #Ground Truth
+                box = image.boxes[row][col]
+                if(box.has_obj):
+                    for obj in box.objs:
+                        true_class = obj.class_num
+                        if(true_class == class_num):
+                            correct += 1
+                        break
+
+
+    return correct*1.0/object_num
+
+def Recall(imageList,model,sample_number=5000,thresh=0.3):
+    correct = 0
+    obj_num = 0
+    count = 0
+    for image in imageList:
+        count += 1
+        #Get prediction from neural network
+        img = crop_detection(image.imgPath,new_width=150,new_height=150)
+        img = numpy.expand_dims(img, axis=0)
+        out = model.predict(img)
+        out = out[0]
+        #for each ground truth, see we have predicted a corresponding result
+        for i in range(49):
+            preds = out[i*25:i*25+25]
+            row = int(i/7)
+            col = int(i%7)
+            box = image.boxes[row][col]
+            if(box.has_obj):
+                for obj in box.objs:
+                    obj_num += 1
+                    true_class = obj.class_num
+                    #see what we predict
+                    if(preds[24] > thresh):
+                        predcit_class = numpy.argmax(preds[4:24])
+                        if(predcit_class == true_class):
+                            correct += 1
+    return correct*1.0/obj_num
+
+def MeasureAcc(model,sample_number,vocPath,imageNameFile):
+    imageList = prepareBatch(0,sample_number,imageNameFile,vocPath)
+    acc = Acc(imageList,model)
+    re = Recall(imageList,model)
+
+    return acc,re
+
+class TestAcc(keras.callbacks.Callback):
+    '''
+    calculate test accuracy after each epoch
+    '''
+
+    def on_epoch_end(self,epoch, logs={}):
+        #Save check points
+        filepath = 'weights'+str(epoch)+'.hdf5'
+        print('Epoch %03d: saving model to %s' % (epoch, filepath))
+       # self.model.save_weights('data'filepath, overwrite=True)
+
+        #Test train accuracy, only on 2000 samples
+        vocPath = 'data/VOC2012'
+        imageNameFile = vocPath+'/ImageSets/Segmentation/val.txt'
+        sample_number = 200
+        acc,re = MeasureAcc(self.model,sample_number,vocPath,imageNameFile)
+        print ('Accuracy and recall on train data is: %3f,%3f'%(acc,re))
+
